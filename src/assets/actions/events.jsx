3f54eb5cab68bace42bc6c797fcd806e0ad73936
Santos Pierre <santospierreemmanuel@gmail.com>
2019-07-24 18:55:40 +0200

    Event Actions

---
+++ src/assets/actions/events.jsx
@@ -0,0 +1,27 @@
+const uuidv4 = require("uuid/v4");
+
+//ADD_Event
+export const addEvent = ({
+  description = "",
+  startDate = 0,
+  endDate =0
+} = {}) => ({
+  type: "ADD_EVENT",
+  Event: {
+    id: uuidv4(),
+    description,
+    startDate,
+    endDate,
+  }
+});
+//REMOVE_Event
+export const removeEvent = ({ id } = {}) => ({
+  type: "REMOVE_EVENT",
+  id: id
+});
+//EDIT_Event
+export const editEvent = (id, updates) => ({
+  type: "EDIT_EVENT",
+  updates,
+  id
+});
